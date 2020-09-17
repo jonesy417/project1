a9c3e6b752fcda07592ead5b1ba4aafb5f216f54
jonesy417 <70974365+jonesy417@users.noreply.github.com>
2020-09-17 12:09:54 -0700

    Add files via upload

---
+++ btcextradata.py
@@ -0,0 +1,33 @@
+#Part 3
+btc_dates = tuple1[0],tuple1[1],tuple1[2],tuple1[3],tuple1[4],tuple1[5],tuple1[6],tuple1[7],tuple1[8],tuple1[9],tuple1[10],tuple1[11],tuple1[12],tuple1[13],tuple1[14]
+print("-----BTC Dates-----")
+print(btc_dates)
+btc_opens = tuple1[15],tuple1[16],tuple1[17],tuple1[18],tuple1[19],tuple1[20],tuple1[21],tuple1[22],tuple1[23],tuple1[24],tuple1[25],tuple1[26],tuple1[27],tuple1[28],tuple1[29]
+print("-----BTC Opens (USD)-----")
+print(btc_opens)
+btc_closes = tuple1[30],tuple1[31],tuple1[32],tuple1[33],tuple1[34],tuple1[35],tuple1[36],tuple1[37],tuple1[38],tuple1[39],tuple1[40],tuple1[41],tuple1[42],tuple1[43],tuple1[44]
+print("-----BTC Closes (USD)-----")
+print(btc_closes)
+btc_highs = tuple1[45],tuple1[46],tuple1[47],tuple1[48],tuple1[49],tuple1[50],tuple1[51],tuple1[52],tuple1[53],tuple1[54],tuple1[55],tuple1[56],tuple1[57],tuple1[58],tuple1[59]
+print("-----BTC Highs (USD)-----")
+print(btc_highs)
+btc_lows = tuple1[60],tuple1[61],tuple1[62],tuple1[63],tuple1[64],tuple1[65],tuple1[66],tuple1[67],tuple1[68],tuple1[69],tuple1[70],tuple1[71],tuple1[72],tuple1[73],tuple1[74]
+print("-----BTC Lows (USD)-----")
+print(btc_lows)
+
+#def btcDataFormat(tuple1):
+#    i = tuple1
+#    if i[0] > i[15]:
+#        print(i)
+#        i++
+#    elif i[15] > i[30]:
+#        print(i)
+#    elif i[30] > i[45]:
+#        print(i)
+#        i++
+#    elif i[45] > i[60]:
+#        print(i)
+#        i++
+#    elif i[60] > i[75]:
+#        print(i)
+#        i++
